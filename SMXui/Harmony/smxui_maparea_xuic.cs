177062a2b4d571dd653ba044ec4902b7ef6ae0b0
Sirillion <sirillion@gmail.com>
2022-07-14 13:59:06 +0200

    Updated to r20.rc4
    ADDED: Button for voice/text on player window.
    CHANGE: Updated references for SMXcore rename.

---
+++ SMXui/Harmony/smxui_maparea_xuic.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+public class XUiC_MapAreaSDX : XUiC_MapArea
+{
+
+    private EntityPlayer localPlayer;
+    private bool bMapInitialized;
+    private XUiV_Texture xuiTexture;
+    private Texture2D mapTexture;
+
+    public override void Init()
+    {
+        base.Init();
+        initMap();
+    }
+    private void initMap()
+    {
+        if (base.xui.playerUI.entityPlayer == null)
+        {
+            return;
+        }
+        localPlayer = base.xui.playerUI.entityPlayer;
+        this.bMapInitialized = true;
+        this.xuiTexture.Material.SetTexture("_MainTex", this.mapTexture);
+        this.xuiTexture.Size = new Vector2i(1140, 710);
+    }
+
+}
